7c412e7b05eb9f562b3e9cc28a9c9f4ef3b3d713
Matheus de Gondra <65628233+Matheus-Gondra@users.noreply.github.com>
2021-08-11 13:25:30 -0300

    Update app.js

--- Bin2Dec/app.js
+++ Bin2Dec/app.js
@@ -8,8 +8,10 @@ function converter() {
         decimal+= parseInt(bin[i]) * Math.pow(2,bin.length-1-i);
 
     }
-    console.log(decimal)
+    return decimal;
 }
+
+
 function verifica(){
     const regex = /^(0|1)+$/;
     const input = document.querySelector("#bin").value;
@@ -18,8 +20,19 @@ function verifica(){
         converter();
         const mensage = document.querySelector("#mensage");
         mensage.classList.add("hidden");
+	const resultado = document.querySelector("#output");
+	resultado.classList.remove("hidden");
+	resultado.classList.add("output-area");
     }else {
         const mensage = document.querySelector("#mensage");
         mensage.classList.remove("hidden");
     }
+}
+
+function bin2dec() {
+	verifica();
+	const output = document.querySelector("#dec");
+	const dec = converter();
+	output.innerHTML = dec;
+	
 }
